46b5c77c3c730bcacc111799446c9baa0bc99573
bjsung <tjdqudwn@gmail.com>
2024-06-29 11:15:21 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat='logo'/>
-      <h2>let's develop management system!</h2>
-
-
-      <h2>let's develop management system!</h2>
-
-
-
-    </div>
-
-  );
+const customers=[
+  {
+    'id' : 1,
+    'image' : 'https://othetak.com/images/desktop/logo/logo_header_ico.svg',
+    'name':'홍길동',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job':'대학생'
+  },  {
+    'id' : 2,
+    'image' : 'https://othetak.com/images/desktop/logo/logo_header_ico.svg',
+    'name':'송수정',
+    'birthday' : '941020',
+    'gender' : '여자',
+    'job':'선생님'
+  },  {
+    'id' : 3,
+    'image' : 'https://othetak.com/images/desktop/logo/logo_header_ico.svg',
+    'name':'성병주',
+    'birthday' : '9108060',
+    'gender' : '남자',
+    'job':'대학생'
+  }
+]
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {customers.map(customersInfo => 
+        {
+          return ( 
+            <Customer
+            key={customersInfo.id}
+            id={customersInfo.id}
+            image={customersInfo.image}
+            name={customersInfo.name}
+            birthday={customersInfo.birthday}
+            gender={customersInfo.gender}
+            job={customersInfo.job}
+            />
+          );
+        })
+        }
+       
+      
+    
+      </div>
+    );
+  }
 }
+
 
 export default App;
